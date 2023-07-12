db7f5ddb477b54e9d726a2355a082411f91b56fb
rhaap <rhaap@rhaap.com>
2023-07-12 14:27:10 -0400

    fix

--- terraform-plan/providers.tf
+++ terraform-plan/providers.tf
@@ -15,10 +15,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id   = "<azure_subscription_id>"
-  tenant_id         = "<azure_subscription_tenant_id>"
-  client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
 }
 
 
