e04fdde12e59d5e99392c260660a3d53d4af3123
David Cuerrier <david.cuerrier@gmail.com>
2023-07-12 00:12:27 -0400

    fix

--- terraform-plan/main.tf
+++ terraform-plan/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_policy_definition" "common_policies" {
   display_name        = var.common_policy_collection[count.index].name
   # management_group_id = azurerm_management_group.example.id
 
-  policy_rule = file("${path.module}/${var.common_policy_collection[count.index].rule}")
+  policy_rule = file("${var.common_policy_collection[count.index].rule}")
 }
 
 resource "azurerm_policy_definition" "env_specific_policies" {
@@ -29,7 +29,7 @@ resource "azurerm_policy_definition" "env_specific_policies" {
   display_name        = var.env_specific_policy_collection[count.index].name
   # management_group_id = azurerm_management_group.example.id
 
-  policy_rule = file("${path.module}/${var.env_specific_policy_collection[count.index].rule}")
+  policy_rule = file("${var.env_specific_policy_collection[count.index].rule}")
   
 }
 
